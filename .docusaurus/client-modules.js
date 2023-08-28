8c896245c7f3388bd5c3b0ba2814e5b4fe1990f3
Steven Plotkin <Steven.Plotkin@ibm.com>
2023-08-28 16:41:17 -0400

    sp5

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/stevenplotkin/Documents/GitHub/software-networking-pot/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/stevenplotkin/Documents/GitHub/software-networking-pot/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/stevenplotkin/Documents/GitHub/software-networking-pot/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/stevenplotkin/Documents/GitHub/software-networking-pot/src/css/custom.css'),
-];
